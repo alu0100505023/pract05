56bf00b11b04bde6b45dc91def774ac1f156f581
Javier Rodriguez <alu3992@cc418.(none)>
2013-10-18 14:55:38 +0100

    añadida division y mul

---
+++ racional.rb
@@ -0,0 +1,43 @@
+# Implementar en este frichero la clase para crear objetos racionales
+
+require "./gcd.rb"
+
+class Fraccion
+	attr_reader :n, :d
+
+   	def initialize (n, d)
+    		@n, @d = n, d
+  	end
+	
+	def to_s
+    		"#{@n}/#{@d}"
+ 	end
+
+	def *(other)
+    		n = @n * other.d
+    		d = @d * other.n
+    		s = Fraccion.new(n, d)
+    		s
+  	end
+
+ 	def div(other)
+    		n = @n * other.n
+    		d = @d * other.d
+    		s = Fraccion.new(n, d)
+    		s
+  	end
+
+	def +(other)
+    		n = (@n * other.d) + (@d * other.n)
+    		d = @d * other.d
+    		s = Fraccion.new(n, d)
+    		s
+  		end
+  
+ 	def -(other)
+    		n = (@n * other.d) + (@d * other.n)
+    		d = @d * other.d
+    		s = Fraccion.new(n, d)
+    		s
+  	end
+end
